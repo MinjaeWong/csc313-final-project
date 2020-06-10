ed3f3fc8a4d3b4cf2059199e9e32f0408cb9d582
MinjaeWong <108051200+MinjaeWong@users.noreply.github.com>
2020-06-10 15:51:31 +0000

    rbd: send header version when notifying

--- classes/BitStamp.php
+++ classes/BitStamp.php
@@ -33,4 +33,5 @@ class BitStamp {
     public function bitstamp_query($path, $req = [], $verb = 'post') {
 
         // generate a nonce as microtime, with as-string handling to avoid problems with 32bits systems
-        $mt
+        $mt = explode(' ', microtime());
+        $r
