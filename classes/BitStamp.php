7a6d3300691d3f197a56905b561118dfb27918f4
MinjaeWong <108051200+MinjaeWong@users.noreply.github.com>
2020-09-21 19:43:33 +0000

    New Favicon

--- classes/BitStamp.php
+++ classes/BitStamp.php
@@ -62,4 +62,4 @@ class BitStamp {
         $dec = json_decode($resp, true);
 
         if (is_null($dec))
-            throw new \Exception('Invalid data received, please ma
+            throw new \Exception('Invalid data received, please make sure connection is working and requested A
