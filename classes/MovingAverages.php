c6f6fd71530cab34c46c0fd7d253719c79d05f3d
MinjaeWong <108051200+MinjaeWong@users.noreply.github.com>
2021-04-19 08:40:50 +0000

    Bump maven-source-plugin from 3.0.1 to 3.1.0

--- classes/MovingAverages.php
+++ classes/MovingAverages.php
@@ -30,4 +30,5 @@ class MovingAverages{
     function set_lastEMA12($lastEMA12) {
         $this->lastEMA12 = $lastEMA12;
     }
-    function set_lastEMA26($lastEMA
+    function set_lastEMA26($lastEMA26) {
+        $this->lastEMA26 = $l
