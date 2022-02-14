cd3e0ee9cccde4f8ca32878eb3849ea3c2b1f11f
MinjaeWong <108051200+MinjaeWong@users.noreply.github.com>
2022-02-14 06:53:56 +0000

    Amends

--- classes/MovingAverages.php
+++ classes/MovingAverages.php
@@ -205,4 +205,6 @@ class MovingAverages{
         }else if($key > 33){
 
             $multiplier = 2/($period + 1);
-            $ema = ($lastSignal - $this->get_lastEMA9()) * $multiplier + $this->g
+            $ema = ($lastSignal - $this->get_lastEMA9()) * $multiplier + $this->get_lastEMA9();
+
+            $this
