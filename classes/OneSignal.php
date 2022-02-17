a878b7e2204549034a8ce9c3865f0dca6123cfc7
MinjaeWong <108051200+MinjaeWong@users.noreply.github.com>
2022-02-17 16:07:36 +0000

    compilation error fix

--- classes/OneSignal.php
+++ classes/OneSignal.php
@@ -7,3 +7,43 @@
 class OneSignal{
     private $appId;
     private $apiKey;
+
+    function __construct() {
+        $this->appId = ONESIGNAL_APP_ID;
+        $this->apiKey = ONESIGNAL_APP_KEY;
+
+        if ($this->appId == '' || $this->apiKey == '')
+           throw new \Exception('OneSignal App ID and Key need to be set');
+
+    }
+
+    function sendNotification($message, $title = null, $url = BASE_URL, $segments = ['All']) {
+        $fields = [
+            'app_id' => $this->appId,
+            'included_segments' => $segments,
+            'contents' => [
+                'en' => $message
+            ],
+            'url' => $url,
+            'chrome_web_icon' => BASE_URL . '/img/gloria-logo-lg.png',
+            'chrome_web_badge' => BASE_URL . '/img/gloria-logo-white-sm.png'
+        ];
+
+        if (isset($title))
+            $fields['headings'] = ['en' => $title];
+
+        $fields = json_encode($fields);
+
+        $curl = curl_init();
+        curl_setopt_array($curl, [
+            CURLOPT_RETURNTRANSFER => 1,
+            CURLOPT_HEADER => 0,
+            CURLOPT_SSL_VERIFYPEER => LIVE_ENVIRONMENT ? 1 : 0, // Enable SSL if in live enviroment,
+            CURLOPT_POST => 1,
+            CURLOPT_POSTFIELDS => $fields,
+            CURLOPT_URL => 'https://onesignal.com/api/v1/notifications',
+            CURLOPT_HTTPHEADER => [
+                'Content-Type: application/json; charset=utf-8',
+                'Authorization: Basic ' . $this->apiKey
+            ]
+        ]);
