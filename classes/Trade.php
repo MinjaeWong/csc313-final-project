62f51c0613d736a7d6e8d1dffe2e8ab2cb035ce1
MinjaeWong <108051200+MinjaeWong@users.noreply.github.com>
2022-02-22 19:35:20 +0000

    Added travis build

--- classes/Trade.php
+++ classes/Trade.php
@@ -69,3 +69,36 @@ class Trade{
             echo "<p>[$buyDate] <span class='blue'>'BUY'</span> &nbsp;: <span class='pink'>$$buyPrice</span></p>";
 
             if($sellDate >0) {
+                $g = new Gloria();
+                $profit = $g->calculateProfitPercentage($buyValue, $buyPrice, $sellPrice);
+                $profitPerc = number_format($profit, 2, '.', '');
+                $totalProfit += $profit;
+                echo "<p>[$sellDate] <span class='blue'>'SELL'</span> : <span class='pink'>$$sellPrice</span></p>";
+                echo "<p>[$sellDate] <span class='".($profitPerc > 0 ? 'green' : 'red')."'>'PROFIT'</span> : <span class='".($profitPerc > 0 ? 'green' : 'red')."'>".($profitPerc > 0 ? '+' : '')."$profitPerc%</span></p><br/>";
+            }
+        }
+
+        $date1 = new DateTime($firstBuy);
+        $date2 = new DateTime();
+        $dateDiff = $date1->diff($date2);
+        $dateDiff = $dateDiff->days;
+
+        echo "<br/><p>";
+        echo "<span class='blue'>Profit Total (Last $dateDiff Days):</span> <span class='".(number_format($totalProfit, 2, '.', '') > 0 ? 'green' : 'red')."'>".(number_format($totalProfit, 2, '.', '') > 0 ? '+' : '').number_format($totalProfit, 2, '.', '')."%</span><br/>";
+        echo "<span class='blue'>Profit-Per-Day:</span> <span class='".(number_format(($totalProfit / $dateDiff), 2, '.', '') > 0 ? 'green' : 'red')."'>".(number_format(($totalProfit / $dateDiff), 2, '.', '') > 0 ? '+' : '').number_format(($totalProfit / $dateDiff), 2, '.', '')."%</span>";
+        echo "</p>";
+
+        return true;
+    }
+
+    function calculateTradeProfit($buyPrice, $sellPrice, $tradeFee){
+
+        $lastTradeBuyFee  = $buyPrice  * $tradeFee;
+        $lastTradeSellFee = $sellPrice * $tradeFee;
+        $totalFees        = $lastTradeBuyFee + $lastTradeSellFee;
+        $lastTradeNet   = $sellPrice - $buyPrice - $totalFees;
+
+        return ($lastTradeNet / $buyPrice) * 100;
+    }
+
+    function get_profitReport(){
