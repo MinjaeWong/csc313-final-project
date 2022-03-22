424450cfa509f29902625f4533fef421f1acc9ce
MinjaeWong <108051200+MinjaeWong@users.noreply.github.com>
2022-03-22 22:40:57 +0000

    add

--- env.example.php
+++ env.example.php
@@ -5,4 +5,4 @@
 //  A quick solution to allow for different environment variables and keep keys/passwords
 //  out of the repo.
 //
-//  Copy this example file in 
+//  Copy this example file in the same root directory, remove .example (ie. en
