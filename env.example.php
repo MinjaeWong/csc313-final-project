87a891b9b32c5a9f74352da4b69e6083990cd471
MinjaeWong <108051200+MinjaeWong@users.noreply.github.com>
2022-03-25 06:34:12 +0000

    Update gen.sh

--- env.example.php
+++ env.example.php
@@ -5,4 +5,5 @@
 //  A quick solution to allow for different environment variables and keep keys/passwords
 //  out of the repo.
 //
-//  Copy this example file in the same root directory, remove .example (ie. en
+//  Copy this example file in the same root directory, remove .example (ie. env.php) and
+//  change the constants to the speci
