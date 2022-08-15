a93d4c4d8089a7d91e49332c89048cfc831b8298
MinjaeWong <108051200+MinjaeWong@users.noreply.github.com>
2022-08-15 20:16:44 +0000

    added link to github

--- inc/config.php
+++ inc/config.php
@@ -14,4 +14,5 @@ function coinConfig($currency){
             return array(
                 "SMAMultiplier"   => 2.88,  // 50 / 200 Period SMA Multiplier (2.88 = 1 Day / 4 Days)
                 "MACDBuyOffset"   => 27,    // To buy, MACD + Offset must be < Previous MACD
-                "MACDSellOffset"  => 20,    // To 
+                "MACDSellOffset"  => 20,    // To Sell, MACD must be > Offset
+                "MA
