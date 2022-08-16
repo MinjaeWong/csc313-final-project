6435e0003da3d93149fc69964a8fe6f49de3b23e
MinjaeWong <108051200+MinjaeWong@users.noreply.github.com>
2022-08-16 20:20:18 +0000

    Update .travis.sh

--- inc/config.php
+++ inc/config.php
@@ -16,4 +16,4 @@ function coinConfig($currency){
                 "MACDBuyOffset"   => 27,    // To buy, MACD + Offset must be < Previous MACD
                 "MACDSellOffset"  => 20,    // To Sell, MACD must be > Offset
                 "MACDMinimum"     => -20,   // To buy, MACD must be < Minimum
-                "Vo
+                "VolAvMultiplier" => 0.02   // % of Average Volum
