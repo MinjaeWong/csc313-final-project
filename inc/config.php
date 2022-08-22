1884b05fcfccc5360a7f895549e185137af0f2bf
MinjaeWong <108051200+MinjaeWong@users.noreply.github.com>
2022-08-22 12:49:31 +0000

    Fix cppcheck warnings

--- inc/config.php
+++ inc/config.php
@@ -23,4 +23,4 @@ function coinConfig($currency){
         case "ltcusd":
             return array(
                 "SMAMultiplier"   => 2.88,  // 50 / 200 Period SMA Multiplier (2.88 = 1 Day / 4 Days)
-                "MACDBuyOffset
+                "MACDBuyOffset"   => 0.5,   // To buy, MACD + Offset must 
