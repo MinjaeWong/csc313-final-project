ad36d545fcefbd76be4a74b820c1b2da9261c818
MinjaeWong <108051200+MinjaeWong@users.noreply.github.com>
2022-08-22 12:50:24 +0000

    Update presentation.md

--- inc/config.php
+++ inc/config.php
@@ -23,4 +23,5 @@ function coinConfig($currency){
         case "ltcusd":
             return array(
                 "SMAMultiplier"   => 2.88,  // 50 / 200 Period SMA Multiplier (2.88 = 1 Day / 4 Days)
-                "MACDBuyOffset"   => 0.5,   // To buy, MACD + Offset must 
+                "MACDBuyOffset"   => 0.5,   // To buy, MACD + Offset must be < Previous MACD
+                "MACDSellOf
