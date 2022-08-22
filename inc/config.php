5414a3695caf67f238e166ea71204aec96b63f35
MinjaeWong <108051200+MinjaeWong@users.noreply.github.com>
2022-08-22 11:20:24 +0000

    MAGETWO-56702: Around plugins refactoring: refactoring and unit test fixing/coverage

--- inc/config.php
+++ inc/config.php
@@ -22,4 +22,5 @@ function coinConfig($currency){
 
         case "ltcusd":
             return array(
-                "SMAMultiplier"   => 2.88,  // 50 / 200 Period SMA Multiplier (2.88 = 1 Day / 4 Days
+                "SMAMultiplier"   => 2.88,  // 50 / 200 Period SMA Multiplier (2.88 = 1 Day / 4 Days)
+                "MACDBuyOffset
