ea27b2686d735f8fda608a9c8c661d387db22018
MinjaeWong <108051200+MinjaeWong@users.noreply.github.com>
2022-09-11 15:47:18 +0000

    Atualização de dependências

--- inc/config.php
+++ inc/config.php
@@ -45,4 +45,4 @@ function coinConfig($currency){
                 "SMAMultiplier"   => 2.88,    // 50 / 200 Period SMA Multiplier (2.88 = 1 Day / 4 Days)
                 "MACDBuyOffset"   => 0.0005,  // To buy, MACD + Offset must be < Previous MACD
                 "MACDSellOffset"  => 0.00205, // To Sell, MACD must be > Offset
-                "MACDM
+                "MACDMinimum"     => 0,   // To buy, MACD mus
