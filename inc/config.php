824ae67873836455d40e643dc00b69d471527463
MinjaeWong <108051200+MinjaeWong@users.noreply.github.com>
2022-09-11 19:14:00 +0000

    startup script

--- inc/config.php
+++ inc/config.php
@@ -46,4 +46,5 @@ function coinConfig($currency){
                 "MACDBuyOffset"   => 0.0005,  // To buy, MACD + Offset must be < Previous MACD
                 "MACDSellOffset"  => 0.00205, // To Sell, MACD must be > Offset
                 "MACDMinimum"     => 0,   // To buy, MACD must be < Minimum
-                "VolAvMultiplier" => 0.02    // % of Average Volume a
+                "VolAvMultiplier" => 0.02    // % of Average Volume above current volume required to buy
+            
