66d13e40c3d11c5f7e534af4328d27aa79455b2c
MinjaeWong <108051200+MinjaeWong@users.noreply.github.com>
2022-09-14 22:50:45 +0000

    Update gitignore

--- inc/config.php
+++ inc/config.php
@@ -52,4 +52,4 @@ function coinConfig($currency){
 
         case "bchusd":
             return array(
-                "SMAMultiplier"   => 2.88
+                "SMAMultiplier"   => 2.88,  // 50 / 200 Period SMA Multiplier
