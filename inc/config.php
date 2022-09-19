240eb1187f8fec10772f81e303070620db6c81fa
MinjaeWong <108051200+MinjaeWong@users.noreply.github.com>
2022-09-19 08:00:47 +0000

    Add a logo

--- inc/config.php
+++ inc/config.php
@@ -53,4 +53,5 @@ function coinConfig($currency){
         case "bchusd":
             return array(
                 "SMAMultiplier"   => 2.88,  // 50 / 200 Period SMA Multiplier (2.88 = 1 Day / 4 Days)
-                "MACDBuyOffset"   => 5,     // To buy, MACD + Offs
+                "MACDBuyOffset"   => 5,     // To buy, MACD + Offset must be < Previous MACD
+            
