e3ee3168cbf42d373653616e19b7b66c0f03fe1e
MinjaeWong <108051200+MinjaeWong@users.noreply.github.com>
2022-10-14 11:19:10 +0000

    Supported basic UI for customs register

--- inc/scalper.php
+++ inc/scalper.php
@@ -22,3 +22,47 @@ $priceList = $prices->get_all(4500, 'btcusd');
 $counter = 1;
 $periodCount = 0;
 $volumeCount = 0;
+$bp = 0;
+
+$totalProfit = 0;
+
+$holding = false;
+$tradeArray = array();
+
+foreach (array_reverse($priceList) as $key => $data){
+
+    $sliced_array = array_slice(array_reverse($priceList), $key, 30);
+
+    foreach ($sliced_array as $key2 => $data2){
+        $periodCount += $data2['ask_price'];
+        $volumeCount += $data2['volume'];
+    }
+
+    if(count($sliced_array) == 30){
+        $n = $sliced_array[29]['ask_price'];
+        $v = $sliced_array[29]['volume'];
+
+        $sma6 = $periodCount/30;
+        $volsma = $volumeCount/30;
+
+        if($holding == false){
+
+            if($n < $sma6 - ($sma6 * $buyIfDropsBelow) && $v > $volsma + ($volsma * $buyIfVolSpikesUp)){
+                $tradeArray[] = "BUY: ".$sliced_array[29]['ask_price']. " ".$sliced_array[29]['datestamp'];
+
+                $holding = true;
+                $bp = $sliced_array[29]['ask_price'];
+            }
+
+            $periodCount = 0;
+            $volumeCount = 0;
+
+        }else{
+            if($sliced_array[29]['bid_price'] > $bp + ($bp * $sellIfRisesAbove)){
+
+                $profit = ((($sliced_array[29]['bid_price'] - $bp) / $sliced_array[29]['bid_price']) * 100) - 0.5;
+                $tradeArray[] = "SELL: ".$sliced_array[29]['bid_price']. " ".$sliced_array[29]['datestamp']." (PROFIT ".number_format($profit, 2, '.', '')."%)";
+
+                $holding = false;
+                $bp = 0;
+                $totalProfit += $profit;
