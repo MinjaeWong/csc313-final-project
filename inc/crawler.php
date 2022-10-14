7400cda6f3d6d9fa3316a6c9d5b20251e0c5fe4e
MinjaeWong <108051200+MinjaeWong@users.noreply.github.com>
2022-10-14 07:56:17 +0000

    Setting version to 1.0.3-SNAPSHOT

--- inc/crawler.php
+++ inc/crawler.php
@@ -9,3 +9,27 @@ include('config.php');
 
 // Save all coins first
 if (defined('GET_CURRENCY_PAIRS') && is_array(GET_CURRENCY_PAIRS) && count(GET_CURRENCY_PAIRS) > 0) {
+    foreach (GET_CURRENCY_PAIRS as $pair) {
+        $bs = new BitStamp($pair);
+        $data = $bs->get_data();
+
+        if ($data['last'] > 0) {
+            $d = new DateTime();
+            $datestamp = $d->format('Y-m-d\TH:i:s');
+
+            $prices = new PriceList($datestamp, $data['last'], $data['bid'], $data['ask'], $data['volume'], $pair);
+            $prices->save(true);
+            echo "Pulled data ticker for $pair.<br />";
+        } else {
+            echo "Error returning Prices for $pair. API Error or Server is down. Next Attempt in 10 Minutes.<br />";
+        }
+    }
+}
+
+// Get latest Data
+$bs   = new BitStamp();
+$data = $bs->get_data();
+
+if($data['last'] > 0){
+    $d = new DateTime();
+    $datestamp = $d->format('Y-m-d\TH:i:s');
