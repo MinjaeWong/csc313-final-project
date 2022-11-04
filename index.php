291b6b3282d106c5d50820a47f800752640dfe72
MinjaeWong <108051200+MinjaeWong@users.noreply.github.com>
2022-11-04 20:12:20 +0000

    Update header.html

--- index.php
+++ index.php
@@ -30,4 +30,4 @@ $profitReport = $profit->get_profitReport();
 <html lang="en">
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-widt
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"
