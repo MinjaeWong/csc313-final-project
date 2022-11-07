b07a4c10f4abe6da5c098b9e88ddbabb03fe0fff
MinjaeWong <108051200+MinjaeWong@users.noreply.github.com>
2022-11-07 18:43:06 +0000

    Kleinigkeit

--- index.php
+++ index.php
@@ -32,4 +32,5 @@ $profitReport = $profit->get_profitReport();
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="robots" content="noindex, nofollow">
-    <link rel="shortcut icon" type="image/png" href
+    <link rel="shortcut icon" type="image/png" href="img/favicon.png"/>
+    <link rel="styleshee
