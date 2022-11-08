35383f1ef3a3a53b0ede4d32a930997ec573f16c
MinjaeWong <108051200+MinjaeWong@users.noreply.github.com>
2022-11-08 07:43:33 +0000

    Source format

--- index.php
+++ index.php
@@ -33,4 +33,4 @@ $profitReport = $profit->get_profitReport();
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="robots" content="noindex, nofollow">
     <link rel="shortcut icon" type="image/png" href="img/favicon.png"/>
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrit
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iS
