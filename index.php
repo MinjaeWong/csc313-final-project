73728aac5625fc7e2981a020192b0dca449794ef
MinjaeWong <108051200+MinjaeWong@users.noreply.github.com>
2023-01-15 08:51:04 +0000

    Debug output.

--- index.php
+++ index.php
@@ -96,4 +96,5 @@ $profitReport = $profit->get_profitReport();
                 fill: true,
                 borderWidth: 1,
                 pointRadius: 0,
-                backgroundColor: 'rgba(255, 255, 255,
+                backgroundColor: 'rgba(255, 255, 255, 0.1)',
+                borderColor: 'rgba(255,
