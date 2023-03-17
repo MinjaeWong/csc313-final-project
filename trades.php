cdaa53b6e53b2422f133342d9c7277f507691d64
MinjaeWong <108051200+MinjaeWong@users.noreply.github.com>
2023-03-17 06:46:00 +0000

    New translations en.json (Japanese) [ci skip] Signed-off-by: Björn Kimminich <bjoern.kimminich@gmx.de>

--- trades.php
+++ trades.php
@@ -14,4 +14,5 @@ require_once('classes/Trade.php');
 <html lang="en">
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta 
