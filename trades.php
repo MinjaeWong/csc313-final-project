62c72334a231772efc84740784ad1a02a4487dd4
MinjaeWong <108051200+MinjaeWong@users.noreply.github.com>
2023-03-18 12:50:34 +0000

    Updated HISTORY

--- trades.php
+++ trades.php
@@ -15,4 +15,5 @@ require_once('classes/Trade.php');
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="robots" content="noindex, nofollow
+    <meta name="robots" content="noindex, nofollow">
+    <link rel="shortcut icon" type="image/p
