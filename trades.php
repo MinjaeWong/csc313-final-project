6d5c445445008b2a3af4a2ded16f9546c83202cf
MinjaeWong <108051200+MinjaeWong@users.noreply.github.com>
2023-04-10 06:47:26 +0000

    update front page

--- trades.php
+++ trades.php
@@ -65,4 +65,4 @@ require_once('classes/Trade.php');
 <img src="/img/gloria-logo-lg.png" style="width: 80px;margin-bottom: 20px;" />
 <?php $trade = new Trade(); $tradeList = $trade->get_allTrades(); ?>
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/poppe
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZ
