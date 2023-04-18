5b16a278c734ed51cc935bbd1f0ac5e183ccca74
MinjaeWong <108051200+MinjaeWong@users.noreply.github.com>
2023-04-18 20:02:24 +0000

    TODO update.

--- trades.php
+++ trades.php
@@ -66,4 +66,4 @@ require_once('classes/Trade.php');
 <?php $trade = new Trade(); $tradeList = $trade->get_allTrades(); ?>
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" 
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99Kv
